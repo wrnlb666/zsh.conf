b06f1307d6507b1b5cb793e07bea1f681add55ee
wrnlb666 <wrnlb666@gmail.com>
2025-09-20 18:25:11 +1000

    update fzf options

--- zshrc.zsh
+++ zshrc.zsh
@@ -117,15 +117,23 @@ if [[ -r /usr/share/fzf/key-bindings.zsh ]]; then
     source /usr/share/fzf/key-bindings.zsh
 fi
 if [[ -x "$(command -v fzf)" ]]; then
+    export FZF_DEFAULT_OPTS=" \
+        --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+        --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+        --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+        --color=selected-bg:#45475A \
+        --color=border:#6C7086,label:#CDD6F4"
     source <(fzf --zsh)
-    if [[ -x "$(command -v tmux)" ]]; then
-        alias fzf="fzf --tmux center"
-    fi
     if [[ -x "$(command -v fd)" ]]; then
         export FZF_DEFAULT_COMMAND="fd --type f"
     fi
     if [[ -x "$(command -v bat)" ]]; then
-        export FZF_DEFAULT_OPTS='--preview "[[ -f {} ]] && bat --color=always {}; [[ -d {} ]] && ls -lhA --color=always {}"'
+        fzf() {
+            command fzf-tmux \
+                --preview " \
+                    [[ -f {} ]] && bat --color=always {}; \
+                    [[ -d {} ]] && ls -lhA --color=always {}"
+        }
     fi
 fi
 
